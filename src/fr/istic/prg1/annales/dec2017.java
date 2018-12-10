b2b38fc160c9c4730f9276e8bf5c1da137d14a81
delasalle-sio-delaunay-p <delasalle.sio.delaunay.pierre@gmail.com>
2018-12-10 13:05:58 +0100

    Refact

--- src/fr/istic/prg1/annales/dec2017.java
+++ src/fr/istic/prg1/annales/dec2017.java
@@ -41,36 +41,5 @@ public class dec2017 {
 	}
 
 	
-	
-    /**
-     * Si value n'appartient pas à array[0..size-1] et size > SIZE_MAX, size est incrementé de 1, value est insere dans
-     * array et les entiers array[0..size] sont triés par ordre croissant.
-     * Sinon array est inhangé.
-     *
-     * @param value
-     *          La valeur à insérer
-     * @return false si value appartient à array[0..size-1] ou si array est complètement rempli;
-     *          true si value n,appartient pas à array[0..size-1]
-     */
-    public boolean insert(int value) {
-        if (value >= 0 && this.size < SIZE_MAX) {
-            int insertPoint = Arrays.binarySearch(this.array, 0, this.size, value);
-            boolean exist = insertPoint >= 0;
-
-            // Ajouter value à array si value ne l'appartient pas,
-            // incrementer size de 1
-            // et trier array
-            if (!exist) {
-                this.array[this.size] = value;
-                this.size++;
-
-                Arrays.sort(this.array, 0, this.size);
-            }
-
-            return !exist;
-        }
-
-        return false;
-    }
     
 }
